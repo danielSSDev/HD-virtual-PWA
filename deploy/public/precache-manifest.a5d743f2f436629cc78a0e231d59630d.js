a085c24da3e7e23ae92572466c981940dd696fb4
Daniel <daniel-santos2000@live.com>
2020-01-28 09:56:26 -0300

    HD virtual finalizado

---
+++ deploy/public/precache-manifest.a5d743f2f436629cc78a0e231d59630d.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  },
+  {
+    "url": "912ec66d7572ff821749319396470bde.svg"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "revision": "e6bb8dd9a8ea895f1e214a5c4e5ad9a2",
+    "url": "index.html"
+  },
+  {
+    "revision": "862a3a01f9a4923317f2",
+    "url": "main.js"
+  },
+  {
+    "revision": "eba87df796ff9186033f8a654b5c923d",
+    "url": "main.js.gz"
+  }
+]);
